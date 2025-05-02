90706a7f00582e7aa363fb7c9402b31354ebfa85
nick fagan <nick@fagan.org>
2025-05-02 15:28:37 -0400

    add cluster run script

---
+++ experiment/cluster/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=vib_replicate
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=4G
+
+module purge
+module load anaconda3/2024.02
+# conda env create -f environment.yml -p /scratch/naf264/conda/pn0
+conda activate /scratch/naf264/conda/pn0
+
+cd /home/naf264/source/explore-variational-rnn/experiment
+
+python vib_replicate.py
